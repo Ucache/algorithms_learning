8e30d701a0f4fe19ae46a55565e8080b10a8871b
Meng Jinglei <mjl8789@qq.com>
2012-09-15 19:32:24 +0800

    acm.hdu.edu.cn

---
+++ let_the_ballon_rise@acm.hdu.c
@@ -0,0 +1,18 @@
+/*************************************************************************
+    > File Name: let_the_ballon_rise@acm.hdu.c
+    > Author: Meng jl
+    > Mail:mjl8789@gmail.com 
+    > Created Time: Sat 15 Sep 2012 07:15:31 PM CST
+ ************************************************************************/
+
+#include<cstdio>
+
+using namespace std;
+
+int main(){
+    int count;
+    string dic
+    while(cin>>count&&count){
+
+    }
+}
